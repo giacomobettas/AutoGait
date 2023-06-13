173b6721037ec796292f38bd4de5c12270dd386d
Giacomo <115986592+giacomobettas@users.noreply.github.com>
2023-06-13 14:36:58 +0000

    Alert functions

--- AutoGait_detection.py
+++ AutoGait_detection.py
@@ -41,10 +41,58 @@ anomalous_frames = [cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR) for frame in anomalo
 
 # Post-processing and Visualization
 
-# Display anomalous frames
-for frame in anomalous_frames:
-    cv2.imshow('Anomaly Detection', frame)
-    cv2.waitKey(1000)  # Show each frame for 1 second
-cv2.destroyAllWindows()
+# Alert function
+def send_alert():
+    cv2.imshow('Anomaly Detected!', np.zeros((100, 300), dtype=np.uint8)) # Displaying a pop-up window using OpenCV
+    cv2.waitKey(3000)  # Display the alert for 3 seconds
+    cv2.destroyAllWindows()
 
-# Alerts
+# Sound alert function
+def play_sound():
+    from playsound import playsound
+    sound_file_path = 'path_to_sound_file.mp3'  # Path to sound file
+    playsound(sound_file_path)
+
+# Email notification function with image attachment
+def send_email_notification(anomalous_frame):
+    import smtplib
+    from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.image import MIMEImage
+
+    # Email configuration
+    sender_email = 'your_email@gmail.com'
+    sender_password = 'your_email_password'
+    receiver_email = 'receiver_email@gmail.com'
+
+    # Email content
+    subject = 'Anomaly Detected!'
+    body = 'Anomalous frame detected. Please check.'
+
+    # Create the email message
+    message = MIMEMultipart()
+    message['From'] = sender_email
+    message['To'] = receiver_email
+    message['Subject'] = subject
+    message.attach(MIMEText(body, 'plain'))
+
+    # Attach the anomalous frame as an image
+    frame_attachment = MIMEImage(anomalous_frame)
+    frame_attachment.add_header('Content-Disposition', 'attachment', filename='anomalous_frame.jpg')
+    message.attach(frame_attachment)
+
+    # Send the email
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.starttls()
+        server.login(sender_email, sender_password)
+        server.send_message(message)
+
+# Anomaly detection loop
+for frame in validation_frames:
+    reconstructed_frame = autogait.predict(np.expand_dims(frame, axis=0))
+    error = np.mean(np.square(frame - reconstructed_frame))
+    
+    if error > threshold:
+        anomalous_frames.append(frame)
+        play_sound()
+        send_email_notification()
