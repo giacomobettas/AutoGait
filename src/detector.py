f93c46b577bd1aa2c11796444956f7608aae65b2
Giacomo <115986592+giacomobettas@users.noreply.github.com>
2023-06-29 10:06:34 +0000

    Detector commit

---
+++ src/detector.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+def detect_anomalies(video_path, autoencoder):
+    cap = cv2.VideoCapture(video_path)
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    anomalies = []
+    threshold = 0.05 # Adjust the threshold value based on requirements
+
+    for i in range(frame_count):
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Preprocess and reshape the frame
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = frame.reshape(1, -1)
+
+        # Predict the reconstructed frame using the autoencoder
+        reconstructed_frame = autoencoder.predict(frame)
+
+        # Calculate the reconstruction error
+        error = np.mean(np.square(frame - reconstructed_frame))
+
+        if error > threshold:
+            anomalies.append(frame)
+
+    cap.release()
+    return anomalies
