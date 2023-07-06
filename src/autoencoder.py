43b7714c89c2a251ad51f3c3f5d9f52e4e2dcdef
Giacomo Bettas-Begalin <giacomo.bettas@gmail.com>
2023-07-06 11:12:32 +0200

    Added model reproducibility

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -12,7 +12,7 @@ def train_autoencoder(input_folder_path, save_path):
     X_train = X_train.reshape(X_train.shape[0], -1)
 
     # Create and train the autoencoder model
-    autoencoder = MLPRegressor(hidden_layer_sizes=(128, 64, 128), activation='relu', max_iter=1000)
+    autoencoder = MLPRegressor(hidden_layer_sizes=(128, 64, 128), activation='relu', random_state=49, max_iter=1000)
     autoencoder.fit(X_train, X_train)
 
     # Save the trained autoencoder model to a file
