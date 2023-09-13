043aeec4e6d0a011f3eb0064870c489ed799cd97
Giacomo Bettas-Begalin <giacomo.bettas@gmail.com>
2023-09-13 20:05:20 +0200

    Save only first and last frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import joblib
 from src.detector import detect_anomalies
-from src.alerting import display_and_alert
+from src.alerting import alert
 
 # Define test video path
 test_video_path = 'data/test_video.mp4'
@@ -12,5 +12,5 @@ saved_autoencoder = joblib.load('models/autoencoder_model.pkl')
 anomalies = detect_anomalies(test_video_path, saved_autoencoder)
 
 # Display alerts
-display_and_alert(anomalies)
+alert(anomalies, 'test')
 # display_and_alert(anomalies, 'recipient@example.com')
