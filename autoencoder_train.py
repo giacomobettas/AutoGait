5eaacf3c57d7d504f8c6abdbae7881a6665592b7
Giacomo <115986592+giacomobettas@users.noreply.github.com>
2023-09-21 11:30:41 +0200

    Update autoencoder_train.py

--- autoencoder_train.py
+++ autoencoder_train.py
@@ -1,22 +1,12 @@
-import joblib
-# import sys
+import os
 from src.autoencoder import train_autoencoder
+from tensorflow.keras.models import load_model
 
-# Define the video paths
+# Define the input videos folder
 input_folder_path = 'data/input_videos'
-test_video_path = 'data/test_video.mp4'
 
 # Train the autoencoder and save the model
-autoencoder = train_autoencoder(input_folder_path, 'models/autoencoder_model.pkl')
+autoencoder_model_path = 'models/autoencoder_model.h5'
+train_autoencoder(input_folder_path, autoencoder_model_path)
 
-"""
-# Choose the autoencoder model to use from command line
-if len(sys.argv) < 2:
-    print("Provide the path to the saved model file.")
-    sys.exit(1)
-
-model_path = sys.argv[1]
-"""
-
-# Load the saved autoencoder model
-saved_autoencoder = joblib.load('models/autoencoder_model.pkl')
+# Use the saved autoencoder for further processing (e.g., anomaly detection)
