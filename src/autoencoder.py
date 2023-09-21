8e78700095274a53c752f4008d89cb61019728a1
Giacomo <115986592+giacomobettas@users.noreply.github.com>
2023-09-21 11:27:19 +0200

    CNN-based autoencoder

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -1,24 +1,33 @@
-import joblib
 import numpy as np
-from sklearn.neural_network import MLPRegressor
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Flatten, Reshape, Conv2DTranspose
 from src.video_utils import load_video_frames
 
 def train_autoencoder(input_folder_path, save_path):
     # Load video frames from the folder
     frames = load_video_frames(input_folder_path)
-    
-    # Convert frames to numpy array and flatten each frame
+
+    # Convert frames to numpy array (2D)
     X_train = np.array(frames)
-    num_frames, height, width = X_train.shape
-    X_train = X_train.reshape(num_frames, -1)
-    
-    # Create and train the autoencoder model
-    autoencoder = MLPRegressor(hidden_layer_sizes=(128, 64, 128),
-                               activation='relu', random_state=49, max_iter=1000,
-                               early_stopping=True, verbose=True)
-    autoencoder.fit(X_train, X_train)
+
+    # Create and train the autoencoder model (CNN-based)
+    autoencoder = Sequential()
+
+    # Encoder
+    autoencoder.add(Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=X_train.shape[1:]))
+    autoencoder.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    autoencoder.add(Flatten())
+
+    # Decoder
+    autoencoder.add(Reshape(X_train.shape[1:]))
+    autoencoder.add(Conv2DTranspose(32, (3, 3), activation='relu', padding='same'))
+    autoencoder.add(Conv2DTranspose(64, (3, 3), activation='relu', padding='same'))
+    autoencoder.add(Conv2DTranspose(1, (3, 3), activation='sigmoid', padding='same'))
+
+    autoencoder.compile(optimizer='adam', loss='mean_squared_error')
+    autoencoder.fit(X_train, X_train, epochs=50, batch_size=32, verbose=1)
 
     # Save the trained autoencoder model to a file
-    joblib.dump(autoencoder, save_path)
+    autoencoder.save(save_path)
 
     return autoencoder
