9b9aa412fe299b82ed00518ebe8a89e83b43769c
Giacomo <115986592+giacomobettas@users.noreply.github.com>
2023-09-21 11:49:59 +0200

    Update detector.py

--- src/detector.py
+++ src/detector.py
@@ -7,16 +7,22 @@ def detect_anomalies(test_video_path, saved_autoencoder):
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
     anomalies = []
-    threshold = 0.05 # Adjust the threshold value based on requirements
+    threshold = 0.05  # Adjust the threshold value based on requirements
 
     for i in range(frame_count):
         ret, frame = cap.read()
         if not ret:
             break
 
-        # Preprocess and reshape the frame
+        # Preprocess the frame
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame = frame.reshape(1, -1)
+
+        # Reshape the frame to match the autoencoder's input shape
+        frame = frame.reshape(1, frame.shape[0], frame.shape[1], 1)
+        """ The frame is reshaped to (1, height, width, 1),
+            where 1 is for the number of samples (batch size),
+            height and width are the dimensions of the frame,
+            and 1 is for the number of channels (grayscale) """
 
         # Predict the reconstructed frame using the autoencoder
         reconstructed_frame = saved_autoencoder.predict(frame)
