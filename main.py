9bcdcf26cfb8a86c73246dbb6e1d3c454022ff50
Giacomo <115986592+giacomobettas@users.noreply.github.com>
2023-09-21 11:36:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-import joblib
 from src.detector import detect_anomalies
 from src.alerting import alert
+from tensorflow.keras.models import load_model
 
 # Define test video path
 test_video_path = 'data/test_video.mp4'
 
 # Load the saved autoencoder model
-saved_autoencoder = joblib.load('models/autoencoder_model.pkl')
+autoencoder_model_path = 'models/autoencoder_model.h5'
+saved_autoencoder = load_model(autoencoder_model_path)
 
 # Detect anomalies using the saved autoencoder model
 anomalies = detect_anomalies(test_video_path, saved_autoencoder)
-
 # Print the number of anomalies detected
 print(f'Anomalies detected: {len(anomalies)}')
 
 # Display alerts
-alert(anomalies, 'test')
-# display_and_alert(anomalies, 'recipient@example.com')
+output_root_folder = 'test'  # Specify your desired output folder
+alert(anomalies, output_root_folder)
