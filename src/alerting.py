0e6516666d1122bd30d98d976d482502b5481436
Giacomo <115986592+giacomobettas@users.noreply.github.com>
2023-09-21 11:52:49 +0200

    Update alerting.py

--- src/alerting.py
+++ src/alerting.py
@@ -6,18 +6,16 @@ import numpy as np
 def alert(anomalies, output_root_folder):
 
     if not os.path.exists(output_root_folder):
-        os.makedirs(output_root_folder) # Create the output root folder if it doesn't exist
+        os.makedirs(output_root_folder)  # Create the output root folder if it doesn't exist
 
     for i, anomaly in enumerate(anomalies):
-        anomaly_folder = os.path.join(output_root_folder, f'anomaly_{i + 1}') # Create a folder for each anomaly
+        anomaly_folder = os.path.join(output_root_folder, f'anomaly_{i + 1}')  # Create a folder for each anomaly
         os.makedirs(anomaly_folder, exist_ok=True)
 
         # Save the first frame of the anomaly
         first_frame = anomaly[0]
 
-        first_frame = first_frame.reshape(240, 320).astype(np.uint8) # Reshape the frame to its original dimensions
-        # first_frame = cv2.cvtColor(first_frame, cv2.COLOR_GRAY2BGR) # Convert to BGR format
-
+        first_frame = first_frame.reshape(first_frame.shape[1], first_frame.shape[2]).astype(np.uint8)  # Reshape the frame to its original dimensions
         first_frame_name = 'first_frame.jpg'
         first_frame_path = os.path.join(anomaly_folder, first_frame_name)
         cv2.imwrite(first_frame_path, first_frame)
@@ -25,21 +23,7 @@ def alert(anomalies, output_root_folder):
         # Save the last frame of the anomaly
         last_frame = anomaly[-1]
 
-        last_frame = last_frame.reshape(240, 320).astype(np.uint8) # Reshape the frame to its original dimensions
-        # last_frame = cv2.cvtColor(last_frame, cv2.COLOR_GRAY2BGR) # Convert to BGR format
-
+        last_frame = last_frame.reshape(last_frame.shape[1], last_frame.shape[2]).astype(np.uint8)  # Reshape the frame to its original dimensions
         last_frame_name = 'last_frame.jpg'
         last_frame_path = os.path.join(anomaly_folder, last_frame_name)
         cv2.imwrite(last_frame_path, last_frame)
-
-"""     for frame in anomalies:
-        
-        # Display the frame
-        cv2.imshow('Anomaly Detected', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-        # Save the frame as an image
-        cv2.imwrite('test/anomaly.jpg', frame)
-
-    cv2.destroyAllWindows() """
